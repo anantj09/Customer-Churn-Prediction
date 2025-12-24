4238e95175145ea33a723b4dbc8bcd00313bf7ab
anant <anantjainaj2005@gmail.com>
2025-12-24 13:18:41 +0530

    Updated files

--- app.py
+++ app.py
@@ -3,23 +3,45 @@ import pandas as pd
 import streamlit as st
 import tensorflow as tf
 import pickle
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
-model = tf.keras.models.load_model('model.h5', compile=False)
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+st.set_page_config(page_title='Customer Analytics', layout='wide')
 
-with open('label_encoder_gender.pkl', 'rb') as file:
-    label_encoder = pickle.load(file)
-with open('one_hot_encoder_geo.pkl', 'rb') as file:
-    ohe_encoder = pickle.load(file)
+@st.cache_resource
+def load_models():
+    m1 = tf.keras.models.load_model('classificationmodel.h5', compile=False)
+    m1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    m2 = tf.keras.models.load_model('regressionmodel.h5', compile=False)
+    m2.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mae'])
+    return m1, m2
 
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+@st.cache_resource
+def load_preprocessing():
+    with open('label_encoder_gender.pkl', 'rb') as f:
+        le = pickle.load(f)
+    with open('one_hot_encoder_geo.pkl', 'rb') as f:
+        ohe = pickle.load(f)
+    with open('class_scaler.pkl', 'rb') as f:
+        c_sc = pickle.load(f)
+    with open('reg_scaler.pkl', 'rb') as f:
+        r_sc = pickle.load(f)
+    return le, ohe, c_sc, r_sc
 
-st.set_page_config(page_title='Churn Predictor', layout='wide')
+model_churn, model_salary = load_models()
+label_encoder, ohe_encoder, class_scaler, reg_scaler = load_preprocessing()
 
-st.title('💳 Customer Churn Prediction')
-st.write("**What it does:** The ANN-based model predicts customer churn using their demographic and financial data.")
+nav_col, _ = st.columns([1, 2]) 
+with nav_col:
+    app_mode = st.radio("📍 Select Task", 
+                        ["Churn Prediction", "Salary Prediction"], 
+                        horizontal=True)
+
+if app_mode == "Churn Prediction":
+    st.title('💳 Customer Churn Prediction')
+    st.write("Using ANN to predict if a customer will leave the bank.")
+else:
+    st.title('💰 Customer Salary Prediction')
+    st.write("Using ANN to estimate a customer's salary.")
+
 st.divider()
 
 
@@ -29,16 +51,21 @@ with col1:
     geography = st.selectbox('Geography', ohe_encoder.categories_[0])
     gender = st.selectbox('Gender', label_encoder.classes_)
     age = st.slider('Age', 18, 92)
-    balance = st.number_input('Balance')
-    credit_score = st.slider('Credit Score', min_value=300, max_value=900)
+    balance = st.number_input('Balance', min_value=0.0)
+    credit_score = st.slider('Credit Score', 300, 900)
 
 with col2:
-    estimated_salary = st.number_input('Salary')
     tenure = st.slider('Tenure', 0, 10)
     num_of_products = st.slider('Number of Products', 1, 4)
     has_cr_card = st.selectbox('Has Credit Card', [0, 1])
     is_active_mem = st.selectbox('Is an Active Member', [0, 1])
-
+    
+    if app_mode == "Churn Prediction":
+        extra_val = st.number_input('Estimated Salary', min_value=0.0)
+        extra_col_name = 'EstimatedSalary'
+    else:
+        extra_val = st.selectbox('Has Exited?', [0, 1])
+        extra_col_name = 'Exited'
 
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
@@ -49,25 +76,25 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_mem],
-    'EstimatedSalary': [estimated_salary]
+    extra_col_name: [extra_val]
 })
 
 geo_encoded = ohe_encoder.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=ohe_encoder.get_feature_names_out(['Geography']))
-
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-input_data_scaled = scaler.transform(input_data)
-
-prediction = model.predict(input_data_scaled)
-prediction_probab = prediction[0][0]
 
 if st.button('Predict'):
-    prediction = model.predict(input_data_scaled)
-    prediction_probab = prediction[0][0]
-
-    st.write('The probability to churn is ', prediction_probab)
-    if prediction_probab > 0.5:
-        st.write('The customer is likely to churn')
+    if app_mode == "Churn Prediction":
+        input_data_scaled = class_scaler.transform(input_data)
+        prediction = model_churn.predict(input_data_scaled)
+        prob = prediction[0][0]
+        st.subheader(f'Churn Probability: {prob:.2%}')
+        if prob > 0.5:
+            st.error('The customer is likely to churn.')
+        else:
+            st.success('The customer is likely to stay.')
     else:
-        st.write('The customer is not likely to churn')
+        input_data_scaled = reg_scaler.transform(input_data)
+        prediction = model_salary.predict(input_data_scaled)
+        st.subheader(f'Estimated Salary: ${prediction[0][0]:,.2f}')
